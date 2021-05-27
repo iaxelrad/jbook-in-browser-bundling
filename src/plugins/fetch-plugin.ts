cdc66ec55aff1251f24b5dea5a6f8f602f9c5b4c
Itamar Axelrad <iaxelrad@gmail.com>
2021-05-27 14:28:18 +0300

    fixed typo .

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -8,7 +8,7 @@ const fileCache = localForage.createInstance({
 
 export const fetchPlugin = (inputCode: string) => {
   return {
-    name: 'fetchPlugin',
+    name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         if (args.path === 'index.js') {
@@ -18,19 +18,28 @@ export const fetchPlugin = (inputCode: string) => {
           };
         }
 
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path
-        );
+        // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+        //   args.path
+        // );
 
-        if (cachedResult) {
-          return cachedResult;
-        }
-
+        // if (cachedResult) {
+        //   return cachedResult;
+        // }
         const { data, request } = await axios.get(args.path);
-        const loader = args.path.match(/.css$/) ? 'css' : 'jsx';
+
+        const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
+        const contents =
+          fileType === 'css'
+            ? `
+            const style = document.createElement('style');
+            style.innerText = 'body { background-color: "red" }';
+            document.head.appendChild(style);
+          `
+            : data;
+
         const result: esbuild.OnLoadResult = {
-          loader,
-          contents: data,
+          loader: 'jsx',
+          contents,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
         await fileCache.setItem(args.path, result);
