3717439eaab31292f9f5a9d00dce3d2b62fa85f7
Itamar Axelrad <iaxelrad@gmail.com>
2021-05-29 20:08:33 +0300

    Setting the initial value.

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,8 +1,17 @@
+import React from 'react';
 import MonacoEditor from '@monaco-editor/react';
 
-const CodeEditor = () => {
+interface CodeEditorProps {
+  initialValue: string;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue }) => {
   return (
     <MonacoEditor
+      value={initialValue}
+      theme="dark"
+      language="javascript"
+      height="500px"
       options={{
         wordWrap: 'on',
         minimap: { enabled: false },
@@ -13,9 +22,6 @@ const CodeEditor = () => {
         scrollBeyondLastLine: false,
         automaticLayout: true,
       }}
-      theme="dark"
-      language="javascript"
-      height="500px"
     />
   );
 };
