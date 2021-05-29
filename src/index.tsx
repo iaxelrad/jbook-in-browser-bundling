5e1fdcbfcf1ccb0d940543f1cdaf39d7fb33519e
Itamar Axelrad <iaxelrad@gmail.com>
2021-05-29 10:51:51 +0300

    passing code to the iframe.

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ const App = () => {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
   const ref = useRef<any>();
+  const iframe = useRef<any>();
 
   useEffect(() => {
     startService();
@@ -36,13 +37,23 @@ const App = () => {
       },
     });
 
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
 
   const html = `
-    <script>
-      ${code}
-    </script>
+    <html>
+      <head>
+        <body>
+          <div id="root"></div>
+          <script>
+            window.addEventListener('message', (event) => {
+              eval(event.data);
+            }, false)
+          </script>
+        </body>
+      </head>
+    </html>
   `;
 
   return (
@@ -55,7 +66,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe srcDoc={html} sandbox="allow-scripts" />
+      <iframe ref={iframe} srcDoc={html} sandbox="allow-scripts" />
     </div>
   );
 };
