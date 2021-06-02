f767f14ddbfd8f0fbdf87cd98d667ea993363f45
Itamar Axelrad <iaxelrad@gmail.com>
2021-06-02 11:35:58 +0300

    adding an onResize event.

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,11 +1,21 @@
 import './resizable.css';
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
+import { useEffect } from 'react';
 
 interface ResizableProps {
   direction: 'horizontal' | 'vertical';
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  useEffect(() => {
+    const listener = () => {
+      console.log(window.innerWidth, window.innerHeight);
+    };
+    window.addEventListener('resize', listener);
+    return () => {
+      window.removeEventListener('resize', listener);
+    };
+  }, []);
   let resizableProps: ResizableBoxProps;
 
   if (direction === 'horizontal') {
