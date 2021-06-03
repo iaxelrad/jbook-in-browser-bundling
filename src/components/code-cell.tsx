c814429f1b242572de3dc640c7ead39437bb71c2
Itamar Axelrad <iaxelrad@gmail.com>
2021-06-03 20:48:59 +0300

    Extracting Local State

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -3,30 +3,35 @@ import CodeEditor from './code-editor';
 import Preview from './preview';
 import bundle from '../bundler';
 import Resizable from './resizable';
+import { Cell } from '../state';
+import { useActions } from '../hooks/use-actions';
+interface CodeCellProps {
+  cell: Cell;
+}
 
-const CodeCell = () => {
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const [code, setCode] = useState('');
-  const [input, setInput] = useState('');
   const [error, setError] = useState('');
+  const { updateCell } = useActions();
 
   useEffect(() => {
     const timer = setTimeout(async () => {
-      const output = await bundle(input);
+      const output = await bundle(cell.content);
       setCode(output.code);
       setError(output.err);
     }, 750);
     return () => {
       clearTimeout(timer);
     };
-  }, [input]);
+  }, [cell.content]);
 
   return (
     <Resizable direction="vertical">
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
         <Resizable direction="horizontal">
           <CodeEditor
-            initialValue="const a=1;"
-            onChange={value => setInput(value)}
+            initialValue={cell.content}
+            onChange={value => updateCell(cell.id, value)}
           />
         </Resizable>
         <Preview code={code} error={error} />
